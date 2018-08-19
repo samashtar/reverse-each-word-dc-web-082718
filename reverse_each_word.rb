5c2c0fc386b413ac97df71df38c6fd544d5ecdfd
Sam Ashtar <samashtarm@gmail.com>
2018-08-19 22:27:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   reversed_array = []
-  sentence.split(" ").collect do |word|
+  sentence.split(" ")
+  sentence.collect do |word|
     reversed_array << word.reverse
   end
   reversed_array.join(" ")
