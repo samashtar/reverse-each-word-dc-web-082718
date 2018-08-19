181b9ff3b05aeceed4e75e4ba7c65f4b1bf531be
Sam Ashtar <samashtarm@gmail.com>
2018-08-19 22:19:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@
 
 def reverse_each_word (string)
   newsentence =[]
-  sentence = string.split ("")
- sentence.collect do |word|
+  sentence = string.split ("").collect do |word|
     newsentence << word.reverse
   end 
   newsentence.join
