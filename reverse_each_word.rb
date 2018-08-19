c435cdcde20cfd2155251e5bb82e400ae8ca468f
Sam Ashtar <samashtarm@gmail.com>
2018-08-19 22:27:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   reversed_array = []
-  sentence.split(" ")
-  sentence.collect do |word|
+  sentence.split(" ").collect do |word|
     reversed_array << word.reverse
   end
   reversed_array.join(" ")
